035ebeda6d1078b8eb71acae06ac2c3cae4d1d31
Alexandra <fossum.alexandra@gmail.com>
2019-05-13 21:49:25 -0500

    finished exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,51 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+//        System.out.println("Hello, World!");
+
+        int myFavouriteNumber = 69;
+        System.out.println(myFavouriteNumber);
+
+        String myString = "Poppycock";
+        System.out.println(myString);
+
+        float myNumber = 3;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        System.out.println(theNumberThree);
+
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//        System.out.println(x);
+//        System.out.println(y);
+
+        byte giveItAGo = 126;
+        giveItAGo += 5;
+        System.out.println(giveItAGo);
+
+
+
+
+    }
+}
