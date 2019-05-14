66f9cd6f2a86ee88f75a924afbd294aabd804a67
Alexandra <fossum.alexandra@gmail.com>
2019-05-14 14:36:10 -0500

    refactored into for loops

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,28 +3,49 @@ import java.util.*;
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-        int i = 5;
+//        int i = 5;
+//
+//        while (i <= 15) {
+//            System.out.println(i++);
+//        }
+//
+//        System.out.println("************");
+//
+//        int y = 100;
+//
+//        do {
+////            System.out.println(y += 2);
+//            System.out.println(y -= 5);
+//        } while (y < 100 && y > -5);
+//
+//        System.out.println("************");
+//
+//        long x = 2;
+//
+//        do {
+//            System.out.println(x);
+//            x *= x;
+//        } while (x <= 1000000);
 
-        while (i <= 15) {
-            System.out.println(i++);
+        System.out.println("************");
+
+        for (int i = 5; i <=15; i++) {
+            System.out.println(i);
         }
 
         System.out.println("************");
 
-        int y = 100;
-
-        do {
-//            System.out.println(y += 2);
-            System.out.println(y -= 5);
-        } while (y < 100 && y > -5);
+        for (int i = 0; i <= 100; i += 2) {
+            System.out.println(i);
+        }
 
         System.out.println("************");
 
-        long x = 2;
+        for (int i = 100; i > -10; i -=5) {
+            System.out.println(i);
+        }
 
-        do {
-            System.out.println(x);
-            x *= x;
-        } while (x <= 1000000);
+
+
     }
 }
